2b2170422b1afc92219c21fcfdf07a124f58599b
DongChen(Lomo) <lomo@lomo.space>
2017-12-24 20:26:42 +0800

    new controller

--- app/Http/Controllers/testViewController.php
+++ app/Http/Controllers/testViewController.php
@@ -10,3 +10,32 @@
  * Description:
  *
  */
+
+    namespace  App\Http\Controllers;
+
+    use App\Member;
+    use app\testViewModel;
+
+    class testViewController extends Controller {
+
+//        public $x;
+//
+//        public function __construct()
+//        {
+//            $this->x = 123;
+//        }
+
+        public function testViews() {
+//            return Member::getMember();
+
+//            return view('test2', [
+//                'other' => Member::getMember()
+//            ]);
+
+            return view('testView', [
+                'name' => 'Lomo',
+                'age' => '20',
+                'other' => Member::getMember()
+            ]);
+        }
+    }
