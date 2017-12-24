3212b4bea144aeaba1205faf9ce13f02e7b89d5b
DongChen(Lomo) <lomo@lomo.space>
2017-12-24 20:26:02 +0800

    member model

---
+++ resources/views/test2.blade.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * test2.blade.php
+ *
+ * Author: Lomo
+ * Date: 2017-12
+ * Email: lomo@lomo.space
+ * URL: http://lomo.space
+ *
+ * Description:
+ *
+ */
---
+++ resources/views/testView.blade.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * testView.blade.php
+ *
+ * Author: Lomo
+ * Date: 2017-12
+ * Email: lomo@lomo.space
+ * URL: http://lomo.space
+ *
+ * Description:
+ *
+ */
