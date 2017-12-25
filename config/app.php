552aed62c9ff165429236130a9692e0eb85bed72
DongChen(Lomo) <lomo@lomo.space>
2017-12-25 15:08:23 +0800

    add inject in app for ide-helper

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //for ide plugin
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
