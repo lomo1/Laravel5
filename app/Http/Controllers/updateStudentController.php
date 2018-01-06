61c7946b8a7664ee029d97185a54aa0485553f9c
DongChen(Lomo) <lomo@lomo.space>
2018-01-07 02:03:34 +0800

    ORM更新数据

--- app/Http/Controllers/updateStudentController.php
+++ app/Http/Controllers/updateStudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\StudentModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,5 +13,20 @@ class updateStudentController extends Controller
     {
         $bool = DB::update('update student set age = ? where id = ?', [$newAge, $id]);
         var_dump($bool);
+    }
+
+
+    //使用ORM 更新数据
+
+    public function ormUpdate()
+    {
+        //通过模型 去更新数据
+        //$s= StudentModel::find(9);
+        //$s->name = "new_lool";
+        //var_dump($s->save());
+
+        //批量更新; 将id>13的所有人年龄改为20岁
+        $num = StudentModel::where('id', '>', 13)->update(['age' => '20']);
+        var_dump($num);
     }
 }
