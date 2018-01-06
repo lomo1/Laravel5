6c68370c2af50396991866da3da3c16b7eb963ee
DongChen(Lomo) <lomo@lomo.space>
2018-01-07 02:03:17 +0800

    ORM删除数据

--- app/Http/Controllers/delStudentController.php
+++ app/Http/Controllers/delStudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\StudentModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -17,5 +18,25 @@ class delStudentController extends Controller
         //$res = DB::table('student')->where('id', '=', $id)->delete();
         //清空表
         //DB::table('student')->truncate(); //该操作不返回任何值
+    }
+
+
+    //模型ORM 删除数据
+    public function ormDelete()
+    {
+        //模型删除
+        //$s= StudentModel::find(16);
+        //$r = $s->delete();
+        //var_dump($r);
+
+
+        //通过主键删除; 删除多个,则传一个数组
+        //$s = StudentModel::destroy(100);
+        ////$s = StudentModel::destroy([100,1001,2000]);
+        //var_dump($s);
+
+        //删除指定条件的数据
+        $r = StudentModel::where('id', '>', '18')->delete();
+        var_dump($r);
     }
 }
